5b16d69dc3f3405f5b6a2fd6ef074ab39e6be8cc
wushanle <wushanle@hotmail.com>
2015-02-20 23:00:01 +0800

    Course work

--- run_analysis.R
+++ run_analysis.R
@@ -10,25 +10,22 @@ y_train = read.table( paste(Dir, "UCI HAR Dataset/train/y_train.txt", sep="") ) 
 features = read.table( paste(Dir, "UCI HAR Dataset/features.txt", sep="") )  # names of measures
 
 
-## Merge dataset
+## Merge test and training dataset
 sub = rbind(sub_test, sub_train)
 x = rbind(x_test, x_train)
 y = rbind(y_test, y_train)
 
-a = c(rep("test", 2947), rep("train", 7352))
-dim(a) = c(length(a), 1)
-
-data = cbind(a[, 1], sub, y, x)
+data = cbind(sub, y, x)
 
 # Name the variables
-colnames(data) = c("Test/Train", "Subject", "Activity", as.character(features[, 2]) )
+colnames(data) = c("Subject", "Activity", as.character(features[, 2]))
 
 ## Extract mean and standard deviation of each measure
 
 m = grep("mean()", colnames(data), fixed = TRUE)
 s = grep("std()", colnames(data), fixed = TRUE)
 
-data = data[, c(1,2,3,sort(union(m,s)))]
+data = data[, c(1,2,sort(union(m,s)))]
 
 ## Name the activities in the data set
 
@@ -39,20 +36,19 @@ data[data[, "Activity"]==4, "Activity"] = "Sitting"
 data[data[, "Activity"]==5, "Activity"] = "Standing"
 data[data[, "Activity"]==6, "Activity"] = "Laying"
 
-## Labels the data set with descriptive variable names
+## Simplify the variable name by removing ()
 a = colnames(data)
 colnames(data) = sub("()", "", a, fix=TRUE)
 
 ## Creates an independent tidy data set with the average of each variable for each activity and each subject
 
+data2 = aggregate(data[, 3]~data[, 1]+data[, 2], data, mean) # Calculate the mean for the 1st measure for each activity and subject
 
-data2 = aggregate(data[, 4]~data[, 2]+data[, 3], data, mean)
-
-for (i in 5:69)
+for (i in 4:68)
 {
-      data2 = cbind(data2, aggregate(data[, i]~data[, 2]+data[, 3], data, mean)[, 3])
+      data2 = cbind(data2, aggregate(data[, i]~data[, 1]+data[, 2], data, mean)[, 3])  # Combind the mean for the 2nd to 66th measures to the dataframe
 }      
 
-colnames(data2) = colnames(data)[2:69]
+colnames(data2) = colnames(data) # Name the variables
 
-write.table(data2, paste(Dir, "TidyData.txt", sep=""), row.name=FALSE)
+write.table(data2, paste(Dir, "TidyData.txt", sep=""), row.name=FALSE)  # Outout the tidy dataset
